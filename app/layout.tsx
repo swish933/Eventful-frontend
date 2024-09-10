8cf3cbcc7694b3c794c703cc5e84c6ae3f0f71e6
Ikem Nomso <ikemnomso@gmail.com>
2024-09-11 00:27:31 +0100

    add react query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import ReactQueryProvider from "@/lib/providers/ReactQueryProvider";
 import { ThemeProvider } from "@/components/theme-provider";
-import { roboto } from "./fonts";
 
 export const metadata: Metadata = {
 	title: "Eventful",
@@ -18,12 +18,12 @@ export default function RootLayout({
 		<html lang='en'>
 			<body
 				className={cn(
-					"min-h-screen bg-background text-foreground antialiased",
-					`${roboto.className}`
+					"min-h-screen bg-background text-foreground dark:bg-background dark:text-foreground antialiased",
+					`font-sans`
 				)}
 			>
 				<ThemeProvider attribute='class' defaultTheme='dark'>
-					{children}
+					<ReactQueryProvider>{children}</ReactQueryProvider>
 				</ThemeProvider>
 			</body>
 		</html>
