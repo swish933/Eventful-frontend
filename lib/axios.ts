bd829e5062174d227bfc5598982ec6416d267b43
Ikem Nomso <ikemnomso@gmail.com>
2024-10-11 11:27:50 +0100

    chore(axios): add request interceptor for axios instance

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,12 +5,22 @@ const axiosInstance = axios.create({
 	timeout: 1000,
 	headers: {
 		"Content-Type": "application/json",
-		Authorization:
-			localStorage && localStorage.getItem("token")
-				? `Bearer ${localStorage.getItem("token")}`
-				: null,
 		accept: "application/json",
 	},
 });
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+	function (config) {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
+);
 
 export { axiosInstance };
