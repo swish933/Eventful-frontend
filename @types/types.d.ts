3c8f988b983d1f173e91f3bd2c187af4774ae31b
Ikem Nomso <ikemnomso@gmail.com>
2024-10-30 14:44:14 +0100

    chore(types): add order interface

--- @types/types.d.ts
+++ @types/types.d.ts
@@ -1,60 +1,72 @@
 export interface IUser {
-  id: string;
-  email: string;
-  username: string;
-  avatar: string;
-  phoneNumber: string;
-  role: string;
-  orders: string[];
-  events: string[];
-  createdAt?: string;
-  updatedAt?: string;
+	id: string;
+	email: string;
+	username: string;
+	avatar: string;
+	phoneNumber: string;
+	role: string;
+	orders: string[];
+	events: string[];
+	createdAt?: string;
+	updatedAt?: string;
 }
 
 export interface IEvent {
-  id: string;
-  name: string;
-  images: string[];
-  description: string;
-  price: number;
-  location: string;
-  startsAt: string;
-  endsAt: string;
-  eventType: string;
-  organizer: string;
-  customers: string[];
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	name: string;
+	images: string[];
+	description: string;
+	price: number;
+	location: string;
+	startsAt: string;
+	endsAt: string;
+	eventType: string;
+	organizer: string;
+	customers: string[];
+	createdAt: string;
+	updatedAt: string;
 }
 
 export interface IPopulatedEvent {
-  id: string;
-  name: string;
-  images: string[];
-  description: string;
-  price: number;
-  location: string;
-  startsAt: string;
-  endsAt: string;
-  eventType: string;
-  organizer: { avatar: string; username: string };
-  customers: string[];
-  createdAt: string;
-  updatedAt: string;
+	id: string;
+	name: string;
+	images: string[];
+	description: string;
+	price: number;
+	location: string;
+	startsAt: string;
+	endsAt: string;
+	eventType: string;
+	organizer: { avatar: string; username: string };
+	customers: string[];
+	createdAt: string;
+	updatedAt: string;
+}
+
+export interface IOrder {
+	id: string;
+	amount: number;
+	qrCode: string;
+	status: string;
+	customer: string;
+	event: Pick<
+		IEvent,
+		"images" | "location" | "name" | "price" | "startsAt" | "eventType"
+	>;
 }
 
 export type UserContextType = {
-  currentUser: IUser | null;
-  updateUser: (u: IUser) => void;
+	currentUser: IUser | null;
+	updateUser: (u: IUser) => void;
 };
 
 export type AuthContextType = {
-  token: string | null;
-  updateToken: (t: string) => void;
-  deleteToken: () => void;
+	token: string | null;
+	updateToken: (t: string) => void;
+	deleteToken: () => void;
 };
 
 export type ApiResponse<T> = {
-  message?: string;
-  payload: T;
+	message?: string;
+	payload: T;
 };
