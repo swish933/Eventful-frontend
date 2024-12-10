482ab14078529aa2eb7fe45f68266172d0908f78
Ikem Nomso <ikemnomso@gmail.com>
2024-12-10 08:17:11 +0100

    chore(role-middleware): add middleware to check user role

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,7 @@
 export const isAuthenticated = (): boolean => {
 	return localStorage.getItem("token") ? true : false;
 };
+
+export const checkRole = (): string | null => {
+	return localStorage.getItem("role");
+};
